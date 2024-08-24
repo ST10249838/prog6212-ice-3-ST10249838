cb066644463b3e242fc729c6eebe6ba57f9437e6
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-08-24 11:48:12 +0200

    InitialCommit

---
+++ prog6212-ice-3-ST10249838/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace prog6212_ice_3_ST10249838
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
