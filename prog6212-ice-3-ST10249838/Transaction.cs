084dc5eaa2d9c6fcb6c82fcb398e82d75702a26d
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-08-24 12:23:52 +0200

    Transaction

---
+++ prog6212-ice-3-ST10249838/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace ICE3
+{
+    public class Transaction
+    {
+        public string TransactionID { get; set; } = Guid.NewGuid().ToString();
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string? Type { get; set; } // "Income" or "Expense"
+        public string? Category { get; set; } // e.g., Groceries, Rent, Salary, etc.
+        public string? Description { get; set; }
+    }
+}
